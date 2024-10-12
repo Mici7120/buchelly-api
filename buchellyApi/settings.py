2ea97024f38f09a1b7ad3c5ed3a4b5877ec914d6
GetialJuan <getial.juan@correounivalle.edu.co>
2024-10-12 18:26:20 -0500

    feat(settings): Remove readonly permissions

--- buchellyApi/settings.py
+++ buchellyApi/settings.py
@@ -138,11 +138,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = []
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
