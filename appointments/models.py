0e0974e63baf2d6e22675936e193509e165e5489
GetialJuan <getial.juan@correounivalle.edu.co>
2024-10-19 21:55:46 -0500

    feat(appointments): Create appointment

--- appointments/models.py
+++ appointments/models.py
@@ -6,14 +6,15 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+import uuid
 
 
 class Appointment(models.Model):
-    appointmentid = models.CharField(db_column='AppointmentId', primary_key=True, max_length=36)  # Field name made lowercase.
+    appointmentid = models.CharField(db_column='AppointmentId', primary_key=True, max_length=36, default=uuid.uuid4())  # Field name made lowercase.
     appuserid = models.ForeignKey('appuser.AppUser', models.DO_NOTHING, db_column='user_id')  # Field name made lowercase.
-    startdatetime = models.DateTimeField(db_column='StartDatetime')  # Field name made lowercase.
-    enddatetime = models.DateTimeField(db_column='EndDatetime')  # Field name made lowercase.
-    status = models.BooleanField(db_column='Status')  # Field name made lowercase.
+    startdatetime = models.DateTimeField(db_column='StartDatetime', unique=True)  # Field name made lowercase.
+    enddatetime = models.DateTimeField(db_column='EndDatetime', unique=True)  # Field name made lowercase.
+    status = models.BooleanField(db_column='Status', default=True)  # Field name made lowercase.
 
     class Meta:
         managed = True
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AppointmentSerializer
+from .models import Appointment
 
 # Create your views here.
+class AppointmentView(viewsets.ModelViewSet):
+    serializer_class = AppointmentSerializer
+    queryset = Appointment.objects.all()
