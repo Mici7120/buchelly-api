7b921f3f50f53656d7828f9ea60b93bfecf95276
GetialJuan <getial.juan@correounivalle.edu.co>
2024-10-19 22:20:47 -0500

    feat(appointments): Not available schedules
    - Method to get all not available schedules
    - The method only returns the schedules not older
        than time now

--- appointments/views.py
+++ appointments/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import AppointmentSerializer
-from .models import Appointment
+from .models import Appointment, Blockeddatetime
+from django.utils import timezone
 
 # Create your views here.
 class AppointmentView(viewsets.ModelViewSet):
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
+
+    @action(detail=False, methods=["get"])
+    def get_not_available_schedules(self, request, pk=None):
+        timenow = timezone.now()
+        blocked_datetimes = Blockeddatetime.objects.filter(
+            status=True,
+            enddatetime__gt=timenow
+        ).values("startdatetime", "enddatetime")
+        appointments = Appointment.objects.filter(enddatetime__gt=timenow).values("startdatetime", "enddatetime")
+
+        not_available_schedules = list(blocked_datetimes) + list(appointments)
+
+        return Response(not_available_schedules)
