6689a93e784e0ebec61874083684ec8ea8e8885f
GetialJuan <getial.juan@correounivalle.edu.co>
2024-10-19 23:20:05 -0500

    feat(settings): Email config
    - Email added

--- appointments/views.py
+++ appointments/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import AppointmentSerializer, BlockeddatetimeSerializer
 from .models import Appointment, Blockeddatetime
 from django.utils import timezone
+from django.core.mail import send_mail
 
 # Create your views here.
 class AppointmentView(viewsets.ModelViewSet):
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
+
+    def perform_create(self, serializer):
+        appointment = serializer.save()
+        send_mail(
+            subject="Creaciones Buchelly - Cita creada",
+            message= f"Su cita ha sido creada exitosamente.\n\nDetalles de la cita:\n\nID: {appointment.appointmentid}\nFecha y hora: {appointment.startdatetime}",
+            recipient_list=[appointment.appuserid.email],
+            from_email=None
+        )
+        return Response(appointment, status=status.HTTP_201_CREATED)
 
     @action(detail=False, methods=["get"])
     def get_not_available_schedules(self, request, pk=None):
