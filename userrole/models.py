0bffc92ff5e066cdc0a146c94bd1c90c26e79d8a
Mici7120 <christian.villanueva@correounivalle.edu.co>
2024-10-25 03:26:56 -0500

    fix userRoleId - default=uuid.uuid4() in UserRole model

--- userrole/models.py
+++ userrole/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+import uuid
 
 class UserRole(models.Model):
-    userroleid = models.CharField(db_column='id', primary_key=True, max_length=36)  # Field name made lowercase.
+    userroleid = models.CharField(db_column='id', primary_key=True, max_length=36, default=uuid.uuid4())  # Field name made lowercase.
     name = models.CharField(db_column='Name', max_length=100)  # Field name made lowercase.
     status = models.BooleanField(db_column='Status')  # Field name made lowercase.
 
